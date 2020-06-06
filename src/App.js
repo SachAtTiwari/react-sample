30bf49d8e42a6d828d1e89dac6063f5a5acf2ce0
sachin <sachin.tiwari50@gmail.com>
2020-06-06 14:42:29 +0530

    first react project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import { FaBeer, FaAccessibleIcon } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+	<FaBeer />
+	<FaAccessibleIcon />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
