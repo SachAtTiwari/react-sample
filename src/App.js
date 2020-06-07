aab905403f90cde14d99f9a14d462eb6f0501511
sachin <sachin.tiwari50@gmail.com>
2020-06-07 11:38:34 +0530

    Test svg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
-import { FaBeer, FaAccessibleIcon } from 'react-icons/fa';
+//import { FaBeer, FaAccessibleIcon } from 'react-icons/fa';
 
 import React, { Component, Fragment } from "react";
-import { render } from "react-dom";
-import ScreenCapture from "./ScreenCapture";
+//import { render } from "react-dom";
+//import ScreenCapture from "./ScreenCapture";
 import "./style.css";
 //import "./test.scss";
 import './App.css';
+import * as rasterizeHTML from 'rasterizehtml';
 
 class App extends Component {
   state = {
@@ -20,10 +21,81 @@ class App extends Component {
     });
   };
 
+
+
+  setInlineStyles = (svg) => {
+ 
+    const explicitlySetStyle = function(element) {
+    let cSSStyleDeclarationComputed = getComputedStyle(element);
+    let i, len, key, value;
+    let svgExcludedValues = ['height', 'width', 'min-height', 'min-width'];
+    let computedStyleStr = "";
+    for (i=0, len=cSSStyleDeclarationComputed.length; i<len; i++) {
+      key=cSSStyleDeclarationComputed[i];
+      if (!((element instanceof SVGElement) && svgExcludedValues.indexOf(key) >= 0)) {
+        value=cSSStyleDeclarationComputed.getPropertyValue(key);
+        computedStyleStr+=key+":"+value+";";
+      }
+    }
+    element.setAttribute('style', computedStyleStr);
+  }
+ 
+  const traverse = function(obj) {
+    let tree = [];
+    tree.push(obj);
+    visit(obj);
+    function visit(node) {
+      if (node && node.hasChildNodes()) {
+        let child = node.firstChild;
+        while (child) {
+          if (child.nodeType === 1 && child.nodeName !== 'SCRIPT'){
+            tree.push(child);
+            visit(child);
+          }
+          child = child.nextSibling;
+        }
+      }
+    }
+    return tree;
+  }
+  // hardcode computed css styles inside SVG
+  let allElements = traverse(svg);
+  let i = allElements.length;
+  while (i--){
+    explicitlySetStyle(allElements[i]);
+  }
+ }
+
+
+ handleClickTakeScreenShot1 = () => {
+   //const { cropPositionTop, cropPositionLeft, cropWidth, cropHeigth } = this.state
+   const body = document.querySelector('body')
+   this.setInlineStyles(body);
+ 
+   var htmlContent = body.innerHTML;
+   // Create a canvas element
+   var canvas = document.createElement('canvas');
+   canvas.setAttribute('id', 'canvas');
+   // Set the width and height of the canvas to match the element's
+   canvas.width = body.getBoundingClientRect().width;
+   canvas.height = body.getBoundingClientRect().height;
+   // Append the canvas to your page, this does not have to be done on the body
+   document.body.appendChild(canvas);
+   // Draw the HTML
+   rasterizeHTML.drawHTML(htmlContent, canvas);
+ 
+ 
+ }
+  
+
   render() {
-    const { screenCapture } = this.state;
+    //const { screenCapture } = this.state;
     return (
-      <ScreenCapture onEndCapture={this.handleScreenCapture}>
+          <Fragment>
+            <button onClick={this.handleClickTakeScreenShot1}>Capture</button>
+            <img src={logo} className="App-logo" alt="logo" />
+          </Fragment>
+      /*<ScreenCapture onEndCapture={this.handleScreenCapture}>
         {({ onStartCapture }) => (
           <Fragment>
             <p>Start editing to see some magic happen :)</p>
@@ -34,13 +106,13 @@ class App extends Component {
            <img src={logo} className="App-logo" alt="logo" />
           </Fragment>
         )}
-      </ScreenCapture>
+      </ScreenCapture>*/
     );
   }
 }
-            //<img width="100"  src='https://svgshare.com/i/LpD.svg' title='' />
 
 
+//<img width="100"  src='https://svgshare.com/i/LpD.svg' title='' />
 //render(<App />, document.getElementById("root"));
 
 
